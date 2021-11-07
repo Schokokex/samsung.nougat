7fdba94795285cce8cbcf1ef1646f8e6c5d37fb3
Joachim <47549862+Schokokex@users.noreply.github.com>
2021-11-07 17:37:31 +0100

    Create debloat.sh

---
+++ debloat.sh
@@ -0,0 +1,11 @@
+pm uninstall --user 0 de.axelspringer.yana.zeropage
+pm uninstall --user 0 com.sec.android.app.voicenote
+pm uninstall --user 0 com.samsung.knox.securefolder
+pm uninstall --user 0 com.samsung.android.videolist
+pm uninstall --user 0 com.samsung.android.video
+pm uninstall --user 0 com.samsung.android.email.provider
+pm uninstall --user 0 com.sec.android.app.fm
+pm uninstall --user 0 com.sec.android.app.music
+
+pm uninstall --user 0 com.samsung.android.messaging
+pm uninstall --user 0 com.sec.phone
